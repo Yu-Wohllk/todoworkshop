f3e1ee5ff428e80a0cc4b386ad885fbe6feb9ff3
Jurgen Wohllk <jurgen.wohllk@scotiabank.cl>
2020-11-17 18:20:15 -0300

    Adding mock of todos

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,43 @@
 import "./App.css";
 import InputComponent from "../InputComponent/InputComponent";
 import Button from "../Button/Button";
-import Todo from '../Todo/Todo';
-import Done from '../Done/Done';
+import Todo from "../Todo/Todo";
+import Done from "../Done/Done";
 
 function App() {
+  const todos = [
+    {
+      id: 1,
+      status: "todo",
+      label: "Comprar Lechuga",
+    },
+    {
+      id: 2,
+      status: "todo",
+      label: "Comprar Tomates",
+    },
+    {
+      id: 3,
+      status: "done",
+      label: "Estudiar Front",
+    },
+    {
+      id: 4,
+      status: "done",
+      label: "Estudiar Android",
+    },
+    {
+      id: 5,
+      status: "done",
+      label: "Ir a Trotar",
+    },
+  ];
+
   return (
     <div className="App">
       <div>
-      <InputComponent />
-      <Button color={'success'}>Guardar</Button>
+        <InputComponent />
+        <Button color={"success"}>Press me</Button>
       </div>
       <div className="TodoDoneContainer">
         <Todo />
